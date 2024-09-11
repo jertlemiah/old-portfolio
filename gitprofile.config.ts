4d99e5cf43f1309e1bd669197572ccb1af2f7ef8
Jeremiah Plauche <67203503+jertlemiah@users.noreply.github.com>
2024-09-11 14:02:23 +0200

    Update gitprofile.config.ts

--- gitprofile.config.ts
+++ gitprofile.config.ts
@@ -12,22 +12,22 @@ const CONFIG = {
   base: '/',
   projects: {
     github: {
-      display: true, // Display GitHub projects?
-      header: 'Github Projects',
-      mode: 'automatic', // Mode can be: 'automatic' or 'manual'
-      automatic: {
-        sortBy: 'stars', // Sort projects by 'stars' or 'updated'
-        limit: 8, // How many projects to display.
-        exclude: {
-          forks: false, // Forked projects will not be displayed if set to true.
-          projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
-        },
-      },
-      // manual: {
-      //   // Properties for manually specifying projects
-      //   projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
-      // },
-    },
+    //   display: true, // Display GitHub projects?
+    //   header: 'Github Projects',
+    //   mode: 'automatic', // Mode can be: 'automatic' or 'manual'
+    //   automatic: {
+    //     sortBy: 'stars', // Sort projects by 'stars' or 'updated'
+    //     limit: 8, // How many projects to display.
+    //     exclude: {
+    //       forks: false, // Forked projects will not be displayed if set to true.
+    //       projects: [], // These projects will not be displayed. example: ['arifszn/my-project1', 'arifszn/my-project2']
+    //     },
+    //   },
+    //   // manual: {
+    //   //   // Properties for manually specifying projects
+    //   //   projects: ['arifszn/gitprofile', 'arifszn/pandora'], // List of repository names to display. example: ['arifszn/my-project1', 'arifszn/my-project2']
+    //   // },
+    // },
     external: {
       header: 'My Projects',
       // To hide the `External Projects` section, keep it empty.
